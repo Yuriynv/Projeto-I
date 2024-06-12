d2d389f36b5fe2b828b1513950f595850f244ff9
Yuriynv <164543230+Yuriynv@users.noreply.github.com>
2024-06-12 14:53:07 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
 
+// formula magica
 float calcular_nota(float custo, float lucro, float imposto) {
-	float valor = custo + lucro;
-	float imposto_rs = valor * imposto / 100;
-	
+	float valor = (custo + lucro);
+	float imposto_rs = (valor * imposto / 100);
+
 	float valor_final = (valor + imposto_rs) / ((100 - imposto) / 100);
-	
+
 	return valor_final;
 }
 
 int main(int argc, char** argv) {
-	
+
 	float custo, lucro, imposto, valor_final;
-	
-	// coletar os dados
-	do {
-		printf("\nDigite o custo: R$");
-		scanf("%f", &custo);
-	} while (custo < 0);
-	
-	do {
-		system("cls");
-		printf("\nCusto Objtido: R$ %.2f", custo);
-		printf("\nDigite o lucro desejado: R$");
-		scanf("%f", &lucro);
-	} while (lucro < 0);
-	
-	do {
-		system("cls");
-		printf("\nCusto Obtido: R$ %.2f", custo);
-		printf("\nLucro Desejado: R$ %.2f", lucro);
-		printf("\nDigite o total de imposto em %: ");
-		scanf("%f", &imposto);
-	} while (imposto < 0);
-	
-	// apresentar os dados
-	system("cls");
-	printf("\nCusto Obtido: R$ %.2f", custo);
-	printf("\nLucro Desejado: R$ %.2f", lucro);
-	printf("\nTotal de Imposto em %: ", imposto);
-	
-	// calcular o valor da nota
+
+	cout << "\n\tCusto: R$ "; cin >> custo;
+	cout << "\n\tLucro desejado: R$ "; cin >> lucro;
+	cout << "\n\tImposto (%): "; cin >> imposto;
+
 	valor_final = calcular_nota(custo, lucro, imposto);
-	
-	printf("\nValor a cobrar: R$ %.2f", valor_final);
-	
+
+	printf("\n\tValor a cobrar: R$ %.2f", valor_final);
+
 	return 0;
 }
